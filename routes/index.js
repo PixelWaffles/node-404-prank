5e884336966da1ee5c278880b568759d8d1367c0
ginsederp <ginsederp@gmail.com>
2015-04-01 19:10:12 +0800

    Set root response to always return 404.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.status(404);
+  res.render('index', { title: '404 Error Not Found'});
 });
 
 module.exports = router;
