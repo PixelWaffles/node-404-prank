ba5906271826ae09a93f47f19dec708984ab9225
ginsederp <ginsederp@gmail.com>
2015-04-01 21:01:36 +0800

    Added words in index route render JSON.

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,12 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.status(404);
-  res.render('index', { title: '404 Error Not Found'});
+  res.render('index', {
+    title: '404 Error Not Found'
+  , word1: 'What. '
+  , word2: "You don't believe me?"
+  , word3: 'Well screw you too.'
+  });
 });
 
 module.exports = router;
